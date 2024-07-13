c0b7a102a56ac4069fb34900b528ca828bcef8e3
odreystella <odreystella.dev@gmail.com>
2024-07-13 23:36:46 +0900

    feat: 유저관련 예외처리 생성

---
+++ src/user/exceptions.py
@@ -0,0 +1,19 @@
+"""
+유저 관련 예외 처리.
+"""
+
+
+class NotAuthorizedException(Exception):
+    message = "Not Authorized"
+
+
+class UserNotFoundException(Exception):
+    message = "User Not Found"
+
+
+class JWTDecodeException(Exception):
+    message = "Invalid Token"
+
+
+class JWTExpiredException(Exception):
+    message = "Expired Token"

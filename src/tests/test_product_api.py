dcffc687ec14ddc829793edfab045b1472dd98e0
odreystella <odreystella.dev@gmail.com>
2024-07-17 15:47:04 +0900

    feat: Product 목록 API 테스트 코드 생성

---
+++ src/tests/test_product_api.py
@@ -0,0 +1,24 @@
+import pytest
+from schema import Schema
+
+from product.models import Product
+
+
+@pytest.mark.django_db
+def test_get_product_list(api_client):
+    """
+    노출된 상품 목록 가져오기 성공 테스트.
+    """
+
+    # given
+    Product.objects.create(name="청바지", price=1, status="active")
+
+    # when
+    response = api_client.get("/products")
+
+    # then
+    assert response.status_code == 200
+    assert len(response.json()["results"]["products"]) == 1
+    assert Schema(
+        {"results": {"products": [{"id": int, "name": "청바지", "price": 1}]}}
+    ).validate(response.json())

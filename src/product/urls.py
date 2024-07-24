4787c169487ea7c09cb1b9ddcab20a68aead4b9c
odreystella <odreystella.dev@gmail.com>
2024-07-24 17:05:17 +0900

    feat: 카테고리 목록 조회 API

--- src/product/urls.py
+++ src/product/urls.py
@@ -1,8 +1,8 @@
 from django.http import HttpRequest
 from ninja import Router
 
-from product.models import Product, ProductStatus
-from product.response import ProductListResponse
+from product.models import Product, ProductStatus, Category
+from product.response import ProductListResponse, CategoryListResponse
 from shared.response import ObjectResponse, response
 
 
@@ -16,10 +16,31 @@ router = Router(tags=["Products"])
     },
 )
 def product_list_handler(request: HttpRequest):
+    """
+    싱품 목록 조회 API
+    """
     return 200, response(
         ProductListResponse(
             products=Product.objects.filter(status=ProductStatus.ACTIVE).values(
                 "id", "name", "price"
             )
+        )
+    )
+
+
+@router.get(
+    "/categories",
+    response={
+        200: ObjectResponse[CategoryListResponse],
+    },
+)
+def categories_list_handler(request: HttpRequest):
+    """
+    카테고리 목록 조회 API
+    N+1쿼리 방지
+    """
+    return 200, response(
+        CategoryListResponse.build(
+            categories=Category.objects.filter(parent=None).prefetch_related("children")
         )
     )

a6e61672fdbc86ebe30a2f7698d520072cd4abee
odreystella <odreystella.dev@gmail.com>
2024-07-25 17:43:03 +0900

    fix: 상품 목록 조회 API에 쿼리파라미터 category_id 추가

--- src/product/urls.py
+++ src/product/urls.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from django.http import HttpRequest
 from ninja import Router
 
@@ -15,17 +17,29 @@ router = Router(tags=["Products"])
         200: ObjectResponse[ProductListResponse],
     },
 )
-def product_list_handler(request: HttpRequest):
+def product_list_handler(request: HttpRequest, category_id: int | None = None):
     """
-    싱품 목록 조회 API
+    쿼리 파라미터인 category_id에 따라 상품 목록 조회 API
     """
-    return 200, response(
-        ProductListResponse(
-            products=Product.objects.filter(status=ProductStatus.ACTIVE).values(
-                "id", "name", "price"
+    if category_id:
+        category: Category | None = Category.objects.filter(id=category_id).first()
+        if not category:
+            products = []
+        else:
+            # category_id: 1 -> [의류.id] + [상의.id, 하의.id]
+            # category_id: 2 -> [상의.id] + []
+            category_ids: List[int] = [category.id] + list(
+                category.children.values_list("id", flat=True)
             )
+            products = Product.objects.filter(
+                category_id__in=category_ids, status=ProductStatus.ACTIVE
+            ).values("id", "name", "price")
+    else:
+        products = Product.objects.filter(status=ProductStatus.ACTIVE).values(
+            "id", "name", "price"
         )
-    )
+
+    return 200, response(ProductListResponse(products=products))
 
 
 @router.get(

182889b0bcabb260c2669f3c2632c385e5523b63
odreystella <odreystella.dev@gmail.com>
2024-07-26 23:17:02 +0900

    fix: full-text search를 위해 Product 모델 수정 및 마이그레이션

--- src/product/models.py
+++ src/product/models.py
@@ -1,7 +1,8 @@
 from enum import StrEnum
 
-
 from django.db import models
+from django.contrib.postgres.search import SearchVectorField
+from django.contrib.postgres.indexes import GinIndex
 
 
 class ProductStatus(StrEnum):
@@ -20,12 +21,17 @@ class Product(models.Model):
     price = models.PositiveIntegerField()
     status = models.CharField(max_length=8)  # active | inactive | paused
     category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+    tags = models.CharField(max_length=128, blank=True)  # 영문 데이터
+    search_vector = SearchVectorField(null=True)  # 형태소 분석 결과를 저장
 
     class Meta:
         app_label = "product"
         db_table = "product"
         indexes = [
             models.Index(fields=["status", "price"]),
+            GinIndex(
+                fields=["search_vector"]
+            ),  # search_vector를 기준으로 역인덱스 생성
         ]
 
 

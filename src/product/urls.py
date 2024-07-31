467821b47a4f0f9ad810fa4beced83bf42a80b95
odreystella <odreystella.dev@gmail.com>
2024-07-31 22:45:46 +0900

    feat: 주문생성API(handler, request, response

--- src/product/urls.py
+++ src/product/urls.py
@@ -1,11 +1,21 @@
-from typing import List
+from typing import List, Dict
 
 from django.http import HttpRequest
+from django.db import transaction
 from ninja import Router
 
-from product.models import Product, ProductStatus, Category
-from product.response import ProductListResponse, CategoryListResponse
-from shared.response import ObjectResponse, response
+from product.models import Product, ProductStatus, Category, Order, OrderLine
+from product.request import OrderRequestBody
+from product.response import (
+    ProductListResponse,
+    CategoryListResponse,
+    OrderDetailResponse,
+)
+from product.exceptions import (
+    OrderInvalidProductException,
+)
+from shared.response import ObjectResponse, response, error_response, ErrorResponse
+from user.authentication import bearer_auth, AuthRequest
 
 
 router = Router(tags=["Products"])
@@ -64,3 +74,55 @@ def categories_list_handler(request: HttpRequest):
             categories=Category.objects.filter(parent=None).prefetch_related("children")
         )
     )
+
+
+@router.post(
+    "/orders",
+    response={
+        201: ObjectResponse[OrderDetailResponse],
+        400: ObjectResponse[ErrorResponse],
+    },
+    auth=bearer_auth,
+)
+def order_products_handler(request: AuthRequest, body: OrderRequestBody):
+    """
+    주문 생성 API
+    Order, OrderLine 트랜잭션으로 처리
+    """
+    product_id_to_quantity: Dict[int, int] = body.product_id_to_quantity
+
+    products: List[Product] = list(
+        Product.objects.filter(
+            id__in=product_id_to_quantity, status=ProductStatus.ACTIVE
+        )
+    )
+    if len(products) != len(product_id_to_quantity):
+        return 400, error_response(msg=OrderInvalidProductException.message)
+
+    with transaction.atomic():
+        total_price: int = 0
+        order = Order.objects.create(user=request.user)
+
+        order_lines_to_create: List[OrderLine] = []
+        for product in products:
+            price: int = product.price
+            discount_ratio: float = 0.9
+            quantity: int = product_id_to_quantity[product.id]
+
+            order_lines_to_create.append(
+                OrderLine(
+                    order=order,
+                    product=product,
+                    quantity=quantity,
+                    price=price,
+                    discount_ratio=discount_ratio,
+                )
+            )
+
+            total_price += price * quantity * discount_ratio
+
+        order.total_price = int(total_price)
+        order.save()
+        OrderLine.objects.bulk_create(objs=order_lines_to_create)
+
+    return 201, response({"id": order.id, "total_price": order.total_price})

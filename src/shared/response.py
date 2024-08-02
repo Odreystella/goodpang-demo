7f0ebc706726695196e89ce9d139a023c423dacc
odreystella <odreystella.dev@gmail.com>
2024-08-02 18:03:05 +0900

    fix: docstring 추가

--- src/shared/response.py
+++ src/shared/response.py
@@ -7,20 +7,40 @@ T = TypeVar("T")
 
 
 def response(results: dict | list | Schema) -> dict:
+    """
+    API 응답 목적으로 사용
+    """
+
     return {"results": results}
 
 
 def error_response(msg: str) -> dict:
+    """
+    API 응답 목적으로 사용
+    """
+
     return {"results": {"message": msg}}
 
 
 class ObjectResponse(Schema, Generic[T]):
+    """
+    스웨거에서 응답 스키마 확인 목적으로 사용
+    """
+
     results: T
 
 
 class ErrorResponse(Schema):
+    """
+    스웨거에서 응답 스키마 확인 목적으로 사용
+    """
+
     message: str
 
 
 class OkResponse(Schema):
+    """
+    스웨거에서 응답 스키마 확인 목적으로 사용
+    """
+
     detail: str = "ok"
